605bd87aa265b26d0a949e965cfd950e0ef17cab
Hard Shah <hardshah2212@gmail.com>
2025-04-08 11:07:10 -0500

    Update CategoryGrid.tsx

--- components/CategoryGrid.tsx
+++ components/CategoryGrid.tsx
@@ -219,7 +219,7 @@ const categories = {
   engineering: {
     icon: <Code size={18} />,
     title: "Production Systems",
-    symbol: "</>",
+    symbol: 🖥️,
     links: [
       {
         id: "eng1",
